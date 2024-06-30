373f4949cca177ff6e949212a54b0021aaf2a9ec
nitishksingh24 <nitishksingh24@gmail.com>
2024-06-30 15:12:18 +0530

    add PC 3

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -14,11 +14,11 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # Applying PCA
-pca = PCA(n_components=2)
+pca = PCA(n_components=3)
 X_pca = pca.fit_transform(X_scaled)
 
 # Creating a DataFrame with PCA results
-df_pca = pd.DataFrame(data=X_pca, columns=['PC1', 'PC2'])
+df_pca = pd.DataFrame(data=X_pca, columns=['PC1', 'PC2', 'PC3'])
 df_pca['Placed'] = y.values
 
 df_pca.to_csv(os.path.join('data','processed','student_performance_pca.csv'), index=False)
